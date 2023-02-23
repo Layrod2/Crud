6af5e0ffdc937a8396475e3d410a30598cb430f1
Ale035 <akumauwo2021@gmail.com>
2023-02-23 18:04:19 -0400

    4 commit

--- src/router/api.js
+++ src/router/api.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const {OnSession,OffSession} = require('../bin/auth.js');
-const Note = require('../schemas/animales.js');
-const Note = require('../schemas/categorias.js');
 
 
 
