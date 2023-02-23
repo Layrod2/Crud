f28510e4b9ff4d9b4bd41101ca7e8afdd5b5d195
Ale035 <akumauwo2021@gmail.com>
2023-02-23 18:33:22 -0400

    commit 5

--- src/bin/db.js
+++ src/bin/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const {Schema} = mongoose;
 const remote = 'mongodb+srv://demon258:j28*Sojo@cluster0.haig3y0.mongodb.net/node-mongodb-apirest?retryWrites=true&w=majority'
-const local = 'mongodb://localhost/animales';
+const local = 'mongodb://mongo:DoTL1o10lWotSyiGgd4m@containers-us-west-175.railway.app:5654';
 
 try {
 	mongoose.connect(local);
